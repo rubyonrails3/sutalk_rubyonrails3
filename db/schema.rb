d9c85e9ee96746dec7ed7bf7792c9228a14b151f
ismail akram <rubyonrails3@gmail.com>
2011-05-15 19:42:27 +0500

    Changed facebook_id from integer to bigint in user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110315184642) do
+ActiveRecord::Schema.define(:version => 20110515143857) do
 
   create_table "presences", :force => true do |t|
     t.integer  "room_id"
