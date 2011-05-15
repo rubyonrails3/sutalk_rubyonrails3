ec328e5c07879f9dccfb4998802688dbbdd61144
ismail akram <rubyonrails3@gmail.com>
2011-05-15 20:08:16 +0500

    removed indexs

--- db/migrate/20110515143857_change_facebook_id_type_to_biginit.rb
+++ db/migrate/20110515143857_change_facebook_id_type_to_biginit.rb
@@ -1,11 +1,9 @@
 class ChangeFacebookIdTypeToBiginit < ActiveRecord::Migration
   def self.up
     change_column :users, :facebook_id, :string 
-    add_index(:users, :facebook_id, :unique => true)
   end
 
   def self.down 
-    remove_index(:users, :facebook_id)
     change_column :users, :facebook_id, :integer
   end
 end
